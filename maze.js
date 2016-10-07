3c5752dd5ed9a62546900b752ba74e7311c3286c
kingdami <icemandelano@hotmail.com>
2016-10-07 12:16:01 -0500

    Add files via upload

---
+++ maze.js
@@ -0,0 +1,5 @@
+window.onload= function(){ 
+	document.getElementById("boundary1").onmouseover= function(){
+		document.getElementById("boundary1").setAttribute("class","boundary youlose");
+	};
+};
